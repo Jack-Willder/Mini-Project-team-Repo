80560700bdda9d46187247399c1fe17b15658f01
Petchi Praveen S <petchipraveen222004@gmail.com>
2025-05-17 15:57:37 +0530

    home page starting

--- praveen/FurnitureManagementsystem/src/App.jsx
+++ praveen/FurnitureManagementsystem/src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import com
+import Header from './components/Header';
 
 function App() {
- 
-
-  return (
- <div>
-
- </div>
+   return (
+<Header />
   )
 }
 
