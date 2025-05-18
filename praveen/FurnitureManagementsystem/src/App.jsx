a35d85c3c78d6d34d512050956aaaac3c4a33cdf
RajK79a <rajk79a@gmail.com>
2025-05-18 19:44:37 +0530

    f

--- praveen/FurnitureManagementsystem/src/App.jsx
+++ praveen/FurnitureManagementsystem/src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from './components/Header';
-import Navbar from "./components/Navbar";
 
-function App(){
-   return(
-   <>
-   <Header />
-   <Navbar />
-   </>
+function App() {
+   return (
+<Header />
   )
 }
 
