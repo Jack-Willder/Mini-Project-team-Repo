d89a1e9146f14cde9e75c8ca3b27811858836191
Petchi Praveen S <petchipraveen222004@gmail.com>
2025-05-20 09:14:51 +0530

    just edited on front page

--- praveen/Furnituremanagementsystem/src/components/Header.jsx
+++ praveen/Furnituremanagementsystem/src/components/Header.jsx
@@ -5,8 +5,11 @@ function Header(){
     <span class="circle-bg">&nbsp;Furniture</span>One
   </h1>
   <ul class="navigation">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Shop</a></li>
+    <li><a href="#">Home &#127968;</a></li>
+    <li><a href="#">Shop 🛒
+</a></li>
+    <li><a href="#">Contact Us 📞
+</a></li>
     <li><a href="#">About</a></li>
     <li><button class="loginbtn">Login</button></li>
   </ul>
