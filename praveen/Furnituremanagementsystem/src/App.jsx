70352c6c17dcb021b30d6054142e45177cf6e037
Petchi Praveen S <petchipraveen222004@gmail.com>
2025-05-22 22:32:48 +0530

    ..

--- praveen/Furnituremanagementsystem/src/App.jsx
+++ praveen/Furnituremanagementsystem/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Body from "./components/Body"
-function App() {
+function App() 
+{
 
   return (
     <>
