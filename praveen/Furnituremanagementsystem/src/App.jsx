1e1745d9220a5d371e74224e7a1797422c4f3e4a
Petchi Praveen S <petchipraveen222004@gmail.com>
2025-06-10 12:04:09 +0530

    10/6/25

--- praveen/Furnituremanagementsystem/src/App.jsx
+++ praveen/Furnituremanagementsystem/src/App.jsx
@@ -6,6 +6,7 @@ import Contact from './pages/Contact';
 import Products from './pages/Products';
 import About from './pages/About'; 
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
